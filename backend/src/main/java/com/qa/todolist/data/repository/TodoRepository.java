7659fa0598112c252aa9d86b1def4d8b9fd97ab5
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 15:26:29 +0000

    TL-99 Init repo for todo item

--- backend/src/main/java/com/qa/todolist/data/repository/TodoRepository.java
+++ backend/src/main/java/com/qa/todolist/data/repository/TodoRepository.java
@@ -1,5 +1,11 @@
 package com.qa.todolist.data.repository;
 
-public class TodoRepository {
+import com.qa.todolist.data.model.Todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
     
 }
