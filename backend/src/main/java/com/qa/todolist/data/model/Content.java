54fbba18dbf5f92cbc45b9ccd026cf4b15276beb
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 12:04:45 +0000

    TL-88 Created id field

--- backend/src/main/java/com/qa/todolist/data/model/Content.java
+++ backend/src/main/java/com/qa/todolist/data/model/Content.java
@@ -1,8 +1,17 @@
 package com.qa.todolist.data.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Content {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_id")
+    private int id;
 
 }
