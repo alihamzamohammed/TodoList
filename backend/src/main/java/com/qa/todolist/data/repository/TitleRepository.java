c197fc9643ebaa91a58417030c640032320f908f
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 15:22:55 +0000

    TL-100 Init repo for titles

--- backend/src/main/java/com/qa/todolist/data/repository/TitleRepository.java
+++ backend/src/main/java/com/qa/todolist/data/repository/TitleRepository.java
@@ -1,5 +1,11 @@
 package com.qa.todolist.data.repository;
 
-public class TitleRepository {
+import com.qa.todolist.data.model.Title;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TitleRepository extends JpaRepository<Title, Integer> {
     
 }
