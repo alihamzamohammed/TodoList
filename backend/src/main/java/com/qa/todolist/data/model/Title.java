05b9db4cc58ed3cae31310ebdd01002ac725f08d
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 12:21:33 +0000

    TL-91 Linked to todo model

--- backend/src/main/java/com/qa/todolist/data/model/Title.java
+++ backend/src/main/java/com/qa/todolist/data/model/Title.java
@@ -4,9 +4,12 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Title {
@@ -18,6 +21,10 @@ public class Title {
 
     @Column(name = "title")
     private String todoTitle;
+
+    @OneToOne(targetEntity = Todo.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_todo_id")
+    private Todo todo;
 
     public int getId() {
         return this.id;
