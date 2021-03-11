7e37751113abb918906610c5c37a24075cd79c4d
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 11:59:11 +0000

    TL-86 Added id field

--- backend/src/main/java/com/qa/todolist/data/model/Title.java
+++ backend/src/main/java/com/qa/todolist/data/model/Title.java
@@ -1,5 +1,23 @@
 package com.qa.todolist.data.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Title {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
