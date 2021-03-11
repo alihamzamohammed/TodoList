f25dfcc0c0f49f974eae7281edc8aa07eea2a8ec
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 12:19:12 +0000

    TL-102 Added Title link

--- backend/src/main/java/com/qa/todolist/data/model/Todo.java
+++ backend/src/main/java/com/qa/todolist/data/model/Todo.java
@@ -1,7 +1,15 @@
 package com.qa.todolist.data.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 public class Todo {
@@ -18,5 +26,9 @@ public class Todo {
     public void setId(int id) {
         this.id = id;
     }
+
+    @OneToOne(mappedBy = "todo", fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Title title;
 
 }
