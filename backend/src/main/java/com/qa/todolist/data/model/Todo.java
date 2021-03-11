782df04a7b691fc62dc40621580e7704d654ef3a
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 12:29:42 +0000

    TL-4 Added required methods

--- backend/src/main/java/com/qa/todolist/data/model/Todo.java
+++ backend/src/main/java/com/qa/todolist/data/model/Todo.java
@@ -1,5 +1,7 @@
 package com.qa.todolist.data.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -34,5 +36,61 @@ public class Todo {
     @OneToOne(mappedBy = "todo", fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Content content;
+
+    public Todo() {
+    }
+
+    public Todo(int id, Title title, Content content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+    public Todo(Title title, Content content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+    public Title getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+
+    public Content getContent() {
+        return this.content;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", title='" + getTitle() + "'" +
+            ", content='" + getContent() + "'" +
+            "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Todo)) {
+            return false;
+        }
+        Todo todo = (Todo) o;
+        return id == todo.id && Objects.equals(title, todo.title) && Objects.equals(content, todo.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content);
+    }
 
 }
