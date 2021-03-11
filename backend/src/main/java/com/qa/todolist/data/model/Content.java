33226951714bdc50d67350f5c61bf743ea160ff3
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 12:27:21 +0000

    TL-90 Added link to todo

--- backend/src/main/java/com/qa/todolist/data/model/Content.java
+++ backend/src/main/java/com/qa/todolist/data/model/Content.java
@@ -2,9 +2,12 @@ package com.qa.todolist.data.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Content {
@@ -16,6 +19,10 @@ public class Content {
 
     @Column(name = "content")
     private String todoContent;
+
+    @OneToOne(targetEntity = Todo.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_todo_id")
+    private Todo todo;
 
     public int getId() {
         return this.id;
@@ -31,6 +38,14 @@ public class Content {
 
     public void setTodoContent(String todoContent) {
         this.todoContent = todoContent;
+    }
+
+    public Todo getTodo() {
+        return this.todo;
+    }
+
+    public void setTodo(Todo todo) {
+        this.todo = todo;
     }
 
 
