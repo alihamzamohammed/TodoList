91893c676cb6f2c76cfaed2870351dc2a98c54c1
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 12:17:06 +0000

    TL-27 Added id

--- backend/src/main/java/com/qa/todolist/data/model/Todo.java
+++ backend/src/main/java/com/qa/todolist/data/model/Todo.java
@@ -1,8 +1,22 @@
 package com.qa.todolist.data.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
 }
