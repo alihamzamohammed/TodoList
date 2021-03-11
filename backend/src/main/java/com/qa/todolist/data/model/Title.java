ede75cbe78c65cb1af4187b82e766e278d00ad25
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-11 12:01:50 +0000

    Added toString, hashcode and equals

--- backend/src/main/java/com/qa/todolist/data/model/Title.java
+++ backend/src/main/java/com/qa/todolist/data/model/Title.java
@@ -33,4 +33,25 @@ public class Title {
         this.todoTitle = todoTitle;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Title)) {
+            return false;
+        }
+        Title title = (Title) o;
+        return id == title.id && Objects.equals(todoTitle, title.todoTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, todoTitle);
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " id='" + getId() + "'" + ", todoTitle='" + getTodoTitle() + "'" + "}";
+    }
+
 }
