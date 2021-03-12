28a75cd7a2ebc21cc263d8ea065db852de49664d
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-12 10:44:40 +0000

    TL-36 Created create function

--- backend/src/main/java/com/qa/todolist/controller/TodoController.java
+++ backend/src/main/java/com/qa/todolist/controller/TodoController.java
@@ -6,10 +6,12 @@ import com.qa.todolist.dto.TodoDTO;
 import com.qa.todolist.service.TodoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,13 @@ public class TodoController {
     public ResponseEntity<TodoDTO> getTodoById(@PathVariable("id") int id) {
         TodoDTO data = todoService.readById(id);
         return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<TodoDTO> createTodo(@Valid @RequestBody Todo todo) {
+        TodoDTO newTodo = todoService.createTodo(todo);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", String.valueOf(newTodo.getId()));
+        return new ResponseEntity<>(newTodo, headers, HttpStatus.CREATED);
     }
 }
