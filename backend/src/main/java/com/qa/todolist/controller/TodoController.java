4d5f24d47fac1c94fa1ad4b9333f60247c23c910
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-12 10:48:47 +0000

    TL-94 Created delete function

--- backend/src/main/java/com/qa/todolist/controller/TodoController.java
+++ backend/src/main/java/com/qa/todolist/controller/TodoController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,11 @@ public class TodoController {
     public ResponseEntity<TodoDTO> updateTodo(@PathVariable("id") int id, @RequestBody Todo todo) {
         TodoDTO updatedTodo = todoService.updateTodo(id, todo);
         return new ResponseEntity<>(updatedTodo, HttpStatus.OK);      
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteTodo(@PathVariable("id") int id) {
+        boolean response = todoService.deleteTodo(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
