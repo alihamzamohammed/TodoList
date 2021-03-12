7462d9f9e121883e6276b4be991bcbbc70a98b0f
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-12 10:41:54 +0000

    TL-35 Added read by id function

--- backend/src/main/java/com/qa/todolist/controller/TodoController.java
+++ backend/src/main/java/com/qa/todolist/controller/TodoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +30,9 @@ public class TodoController {
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoDTO> getTodoById(@PathVariable("id") int id) {
+        TodoDTO data = todoService.readById(id);
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
 }
