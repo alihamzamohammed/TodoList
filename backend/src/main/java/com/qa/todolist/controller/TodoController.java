93d628b6b1c585cdbe0080a7999df740c0d5997d
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-12 10:47:13 +0000

    TL-34 Created update function

--- backend/src/main/java/com/qa/todolist/controller/TodoController.java
+++ backend/src/main/java/com/qa/todolist/controller/TodoController.java
@@ -2,6 +2,9 @@ package com.qa.todolist.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import com.qa.todolist.data.model.Todo;
 import com.qa.todolist.dto.TodoDTO;
 import com.qa.todolist.service.TodoService;
 
@@ -12,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,5 +49,11 @@ public class TodoController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", String.valueOf(newTodo.getId()));
         return new ResponseEntity<>(newTodo, headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TodoDTO> updateTodo(@PathVariable("id") int id, @RequestBody Todo todo) {
+        TodoDTO updatedTodo = todoService.updateTodo(id, todo);
+        return new ResponseEntity<>(updatedTodo, HttpStatus.OK);      
     }
 }
