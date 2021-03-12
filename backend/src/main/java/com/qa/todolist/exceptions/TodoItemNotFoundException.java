b66008880810f972d4058237c965bbf2293bd167
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-12 11:29:08 +0000

    TL-105 Implemented exception

--- backend/src/main/java/com/qa/todolist/exceptions/TodoItemNotFoundException.java
+++ backend/src/main/java/com/qa/todolist/exceptions/TodoItemNotFoundException.java
@@ -1,5 +1,24 @@
 package com.qa.todolist.exceptions;
 
-public class TodoItemNotFoundException {
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Todo item wasn't found")
+public class TodoItemNotFoundException extends EntityNotFoundException{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1424141744361410384L;
+
+
+    public TodoItemNotFoundException() {
+        super();
+    }
     
+    public TodoItemNotFoundException(String message) {
+        super(message);
+    }
 }
