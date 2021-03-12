1b40affce9e9bbe39097ea4f6ad1acd7f94e71d0
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-12 10:39:48 +0000

    TL-95 Created read all function

--- backend/src/main/java/com/qa/todolist/controller/TodoController.java
+++ backend/src/main/java/com/qa/todolist/controller/TodoController.java
@@ -1,8 +1,14 @@
 package com.qa.todolist.controller;
 
+import java.util.List;
+
+import com.qa.todolist.dto.TodoDTO;
 import com.qa.todolist.service.TodoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +22,11 @@ public class TodoController {
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<TodoDTO>> getAllTodos() {
+        List<TodoDTO> data = todoService.readAllTodos();
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
 }
