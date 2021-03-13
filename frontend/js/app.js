f3df4b6bffd0da7e6868dae264c82d8f5a242a98
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-13 15:51:34 +0000

    TL-45 Categories fetched from API, output on body

---
+++ frontend/js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// For dev purposes, fetch link will be hardcoded into functions
+
+let createItem = (categoryId, item) => {
+
+};
+
+
+let createCategory = (category) => {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.textContent = `Category ID: ${category.id}, Category Name: ${category.name}`;
+    div.appendChild(p);
+    document.body.appendChild(div);
+}
+
+let fetchCategories = () => {
+    let request = fetch("http://localhost:8080/category")
+        .then(response => {
+            if (response.status != 200) {
+                console.error(`Error: Status code ${response.status}\n${response.json}`);
+                return;
+            }
+            response.json().then(data => {
+                data.forEach(category => { createCategory(category) });
+            })
+        })
+};
+
+fetchCategories();
