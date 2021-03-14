3c331ee7c0446825311ed091a25f33bb64e6ed48
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-14 12:55:43 +0000

    TL-47 Added dynamic categories from api

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,11 +8,35 @@ let createItem = (categoryId, item) => {
 
 
 let createCategory = (category) => {
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    p.textContent = `Category ID: ${category.id}, Category Name: ${category.name}`;
-    div.appendChild(p);
-    //document.body.appendChild(div);
+    let categoryDiv = document.createElement("div");
+    categoryDiv.classList.add("category");
+    categoryDiv.id = `category-${category.id}`;
+
+    let categoryHeader = document.createElement("div");
+    categoryHeader.classList.add("row");
+    categoryHeader.classList.add("category-header");
+
+    let categoryId = document.createElement("div");
+    categoryId.classList.add("col-1");
+    categoryId.classList.add("category-id");
+
+    let h4 = document.createElement("h4");
+    h4.textContent = `${category.id}`;
+    categoryId.appendChild(h4);
+
+    let categoryName = document.createElement("div");
+    categoryName.classList.add("col-11");
+    categoryName.classList.add("category-name");
+
+    let h3 = document.createElement("h3");
+    h3.textContent = `${category.name}`;
+    categoryName.appendChild(h3);
+
+    categoryHeader.appendChild(categoryId);
+    categoryHeader.appendChild(categoryName);
+    categoryDiv.appendChild(categoryHeader);
+
+    document.querySelector("#container").appendChild(categoryDiv);
 }
 
 let fetchCategories = () => {
