55a32eaba27a9f3c213b2a1d75acf035b189bbea
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-16 12:32:15 +0000

    TL-16 Created js for post request

---
+++ frontend/js/createTodo.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let navToHome = () => {
+    window.location.href = "/frontend/index.html";
+}
+
+let postRequest = async (titleInput, contentInput) => {
+    const response = await fetch("http://localhost:8080/todo", {
+        method: "POST",
+        body: JSON.stringify({
+            title: titleInput,
+            content: contentInput
+        })
+    });
+    if (response.status != 200) {
+        console.error(`Error: Status code ${response.status}\n${response.json}`);
+        return;
+    }
+    let data = await response.json();
+    return data;
+}
+
+let createTodo = () => {
+    let title = document.querySelector("#title-input").textContent;
+    let content = document.querySelector("#content-input").textContent;
+    postRequest(title, content).then((response) => {
+        console.log("done");
+        let p = document.querySelector("#response");
+        p.textContent = `To-Do Item successfully created! Item ID: ${response.id}`;
+        p.style.display = "block";
+    })
+
+}
