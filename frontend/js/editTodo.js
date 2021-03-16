f537ea601c5733a5e78dacd74af2081bf858703b
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-16 16:37:20 +0000

    TL-57 Now fetches categories

--- frontend/js/editTodo.js
+++ frontend/js/editTodo.js
@@ -4,8 +4,8 @@ let navToHome = () => {
     window.location.href = "/frontend/index.html";
 }
 
-let postRequest = async (titleInput, contentInput, categoryInput) => {
-    const response = await fetch("http://localhost:8080/todo", {
+let postRequest = async (id, titleInput, contentInput, categoryInput) => {
+    const response = await fetch(`http://localhost:8080/todo${id}`, {
         method: "PUT",
         headers: {
             'Content-Type': 'application/json'
@@ -26,29 +26,67 @@ let postRequest = async (titleInput, contentInput, categoryInput) => {
     return data;
 }
 
-let createTodo = () => {
+let updateTodo = () => {
     let title = document.querySelector("#title-input").value;
     let content = document.querySelector("#content-input").value;
     let category = parseInt(document.querySelector("#category-selection").value);
     let p = document.querySelector("#response");
     postRequest(title, content, category).then((response) => {
         if (Number.isInteger(response)) {
-            p.textContent = `There was a problem creating your item: Response code ${response}. Check that the server URL is correct, and that the server is running.`;
+            p.textContent = `There was a problem updating your item: Response code ${response}. Check that the server URL is correct, and that the server is running.`;
             p.style.color = "red";
             p.style.display = "block";
         } else {
             console.log(response);
-            p.textContent = `To-Do Item successfully created! Item ID: ${response.id}`;
+            p.textContent = `To-Do Item successfully updated! Item ID: ${response.id}`;
             p.style.color = "green";
             p.style.display = "block";
         }
     }).catch(err => {
         console.error(err);
-        p.textContent = `There was a problem creating your item: ${err}. Check that the server URL is correct, and that the server is running.`;
+        p.textContent = `There was a problem updating your item: ${err}. Check that the server URL is correct, and that the server is running.`;
         p.style.color = "red";
         p.style.display = "block";
     });
+}
 
+let deleteRequest = async (id) => {
+    const response = await fetch(`http://localhost:8080/todo/${id}`, {
+        method: "DELETE"
+    });
+    if (response.status != 200) {
+        console.error(`Error: Status code ${response.status}\n${response.json}`);
+        return response.status;
+    }
+    let data = await response.json();
+    return data;
+}
+
+let deleteTodo = () => {
+    let title = document.querySelector("#title-input").value;
+    let content = document.querySelector("#content-input").value;
+    let category = parseInt(document.querySelector("#category-selection").value);
+    let p = document.querySelector("#response");
+    postRequest(title, content, category).then((response) => {
+        if (Number.isInteger(response)) {
+            p.textContent = `There was a problem deleting your item: Response code ${response}. Check that the server URL is correct, and that the server is running.`;
+            p.style.color = "red";
+            p.style.display = "block";
+        } else {
+            console.log(response);
+            p.textContent = `To-Do Item successfully deleted! Item ID: ${response.id}`;
+            p.style.color = "green";
+            p.style.display = "block";
+            document.querySelector("#title-input").setAttribute("disabled", "true");
+            document.querySelector("#content-input").setAttribute("disabled", "true");
+            document.querySelector("#category-selection").setAttribute("disabled", "true");
+        }
+    }).catch(err => {
+        console.error(err);
+        p.textContent = `There was a problem deleting your item: ${err}. Check that the server URL is correct, and that the server is running.`;
+        p.style.color = "red";
+        p.style.display = "block";
+    });
 }
 
 let getCategories = async () => {
@@ -106,7 +144,10 @@ let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 
 readTodo(urlParams.get("id")).then(data => {
+    document.querySelector("#id").textContent = `ID: ${data.id}`;
     document.querySelector("#title-input").value = data.title;
     document.querySelector("#content-input").value = data.content;
-    document.querySelector("#category-selection").value = `${data.category.id}`
+
+    console.log(data);
+    document.querySelector("#category-selection").value = `${data.category}`
 }).catch(err => console.error(err));
