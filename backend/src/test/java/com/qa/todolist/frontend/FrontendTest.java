455f2ec729777cb921d50b0f909a5af8dbc350c7
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-03-18 21:06:51 +0000

    TL-74 Read category test

--- backend/src/test/java/com/qa/todolist/frontend/FrontendTest.java
+++ backend/src/test/java/com/qa/todolist/frontend/FrontendTest.java
@@ -1,7 +1,14 @@
 package com.qa.todolist.frontend;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.concurrent.TimeUnit;
 
+import com.qa.todolist.data.model.Category;
+import com.qa.todolist.data.model.Content;
+import com.qa.todolist.data.model.Title;
+import com.qa.todolist.data.model.Todo;
+import com.qa.todolist.frontend.home.ReadCategory;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
@@ -11,27 +18,55 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ExtendWith(SeleniumJupiter.class)
+@Sql(scripts = { "classpath:test-schema.sql",
+        "classpath:test-data-todo.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 class FrontendTest {
 
-    private static String frontendURL = "/frontend/";
+    @LocalServerPort
+    private static int port;
+    private static String frontendURL = "http://localhost:5500/frontend/";
     private static WebDriver driver;
     private static ExtentReports extent;
     private static ExtentTest test;
+    // @Autowired
+    // private CategoryRepository categoryRepository;
+    // @Autowired
+    // private TodoRepository todoRepository;
+    // @Autowired
+    // private TitleRepository titleRepository;
+    // @Autowired
+    // private ContentRepository contentRepository;
+    private static Category category;
+    private static Title title;
+    private static Content content;
+    private static Todo todo;
 
     @BeforeAll
     public static void init() {
+        category = new Category("Test Category");
+        title = new Title("Test Title");
+        content = new Content("Test Content");
+        todo = new Todo(title, content, category);
+
         extent = new ExtentReports("src/test/resources/reports/report.html", true);
         test = extent.startTest("ExtentDemo");
-        System.setProperty("webdriver.chrome.driver", "src//test//resources//driver//chromedriver//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src//test//resources//driver//chromedriver.exe");
         ChromeOptions cOptions = new ChromeOptions();
         cOptions.setHeadless(false);
         cOptions.addArguments("--no-sandbox");
@@ -42,6 +77,9 @@ class FrontendTest {
 
         driver = new ChromeDriver(cOptions);
         driver.manage().window().setSize(new Dimension(1280, 720));
+        driver.get(frontendURL + "index.html");
+        driver.manage().addCookie(new Cookie("serverurl", "http://localhost:" + port));
+
     }
 
     @BeforeEach
@@ -50,39 +88,42 @@ class FrontendTest {
     }
 
     @Test
-    public void readCategoryTest() {
+    void readCategoryTest() {
+        driver.get(frontendURL + "index.html");
+        assertThat(ReadCategory.findId(1, driver)).isEqualTo(String.valueOf(1));
+        assertThat(ReadCategory.findName(1, driver)).isEqualTo("test");
     }
 
     @Test
-    public void createCategoryTest() {
+    void createCategoryTest() {
     }
 
     @Test
-    public void editCategoryTest() {
+    void editCategoryTest() {
     }
 
     @Test
-    public void deleteCategoryTest() {
+    void deleteCategoryTest() {
     }
 
     @Test
-    public void readTodoTest() {
+    void readTodoTest() {
     }
 
     @Test
-    public void createTodoTest() {
+    void createTodoTest() {
     }
 
     @Test
-    public void editTodoTest() {
+    void editTodoTest() {
     }
 
     @Test
-    public void deleteTodoTest() {
+    void deleteTodoTest() {
     }
 
     @Test
-    public void backendURLSetTest() {
+    void backendURLSetTest() {
     }
 
     @AfterEach
---
+++ backend/src/test/java/com/qa/todolist/frontend/settings/SettingsPage.java
@@ -0,0 +1,5 @@
+package com.qa.todolist.frontend.settings;
+
+public class SettingsPage {
+
+}
